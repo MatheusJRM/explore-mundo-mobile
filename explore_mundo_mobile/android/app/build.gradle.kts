a17ff8c35c7bb7279c407d9d543af432e99b1ac6
MatheusJRM <matheusjrmoura@gmail.com>
2025-04-08 12:59:03 -0300

    chore: configurações para consumo e produção de .apk

--- explore_mundo_mobile/android/app/build.gradle.kts
+++ explore_mundo_mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.explore_mundo_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
